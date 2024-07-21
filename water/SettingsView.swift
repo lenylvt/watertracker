c99eaf1a29c104206735be8a41dba3e52410acb4
Leny <leny.levant95@icloud.com>
2024-07-21 22:55:12 +0200

    update with each file

---
+++ water/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  water
+//
+//  Created by Leny Levant on 21/07/2024.
+//
+
+import Foundation
---
+++ water/WaterTrackerModel.swift
@@ -0,0 +1,8 @@
+//
+//  WaterTrackerModel.swift
+//  water
+//
+//  Created by Leny Levant on 21/07/2024.
+//
+
+import Foundation
